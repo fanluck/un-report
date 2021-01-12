8846a8033b17a21854e5a186b0c4427c5450012b
Zhaohui Fan <fanluck@med.umich.edu>
2021-01-12 12:07:38 -0500

    Add new codes at the end

--- code/r_for_analysis.R
+++ code/r_for_analysis.R
@@ -59,3 +59,40 @@ Americas_data_2007 <-
 gapminder_data %>%
   filter(continent=="Americas", year==2007) %>% 
   select(-continent, -year)
+
+## Read a new file
+co2_emissions <-read_csv("data/co2-un-data.csv", skip=2,
+         col_names = c("region", "country", "year", "series", "value", "footnotes", "source")) %>%  
+  select(country, year, series, value) %>%  
+  mutate(series = recode(series, "Emissions (thousand metric tons of carbon dioxide)" = "total",
+                         "Emissions per capita (metric tons of carbon dioxide)" = "per_capita")) %>%  
+  pivot_wider(names_from=series, values_from=value) %>% 
+  filter(year==2005) %>%  
+  select(-year) %>% 
+  mutate(country=recode(country,
+                        "Bolivia (Plurin. State of)" = "Bolivia",
+                        "United States of America" = "United States",
+                        "Venezuela (Boliv. Rep. of)" = "Venezuela"))
+
+# Join tables
+anti_join(gapminder_data, co2_emissions, by="country")
+
+
+gapminder_data <- read_csv("data/gapminder_data.csv") %>%
+  filter(year == 2007 & continent == "Americas") %>%
+  select(-year, -continent) %>%
+  mutate(country = recode(country, "Puerto Rico" = "United States")) %>%
+  group_by(country) %>%
+  summarize(lifeExp = sum(lifeExp * pop)/sum(pop), gdpPercap = sum(gdpPercap * pop)/sum(pop), pop = sum(pop))
+
+anti_join(gapminder_data, co2_emissions, by="country")
+
+gapminder_co2 <- inner_join(gapminder_data, co2_emissions, by="country")
+
+ggplot(gapminder_co2, aes(x=gdpPercap, y=per_capita)) +
+  geom_point() +
+  labs(x="GDP (per capita)",
+       y="CO2 emitted (per capita)",
+       title="There is a strong association between a nation's GDP \nand the amount of CO2 it produces"
+  ) +
+  geom_smooth(method="lm")
